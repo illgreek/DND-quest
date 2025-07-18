d6022bba3acab1d584c70c974292d458850a890c
i.hrek <illia.hrek@dreamdev.email>
2025-07-18 12:36:05 +0300

    fixes

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client']
+  }
 };
 
 export default nextConfig;
